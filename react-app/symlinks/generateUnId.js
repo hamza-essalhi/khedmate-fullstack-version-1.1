a434fbce23d7c9898dfd2afc17198eb1cd9e13c5
hamza-essalhi <hamzaessalhix@gmail.com>
2023-09-30 00:19:46 +0100

    all

---
+++ react-app/symlinks/generateUnId.js
@@ -0,0 +1,8 @@
+import crypto from "crypto";
+
+export const generateUniqueId =(id)=> {
+  const len = parseInt(id.length/2)
+  const hash = crypto.createHash('sha256').update(id).digest('hex');
+  return hash.substr(0, len); // Use the first 6 characters of the hash as the unique ID
+
+}
